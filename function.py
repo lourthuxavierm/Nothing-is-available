168f2ab8c9e3d9a585f443ad4497e76a4dd9d48e
lourthuxavierm <xavierstayfoolish@gmail.com>
2023-07-27 12:34:37 +0530

    first commit

--- function.py
+++ function.py
@@ -9,3 +9,10 @@ num1 = 5
 num2 = 10
 result = add_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is: {result}")
+
+
+name ="lourthu xavier m"
+course= "BCA"
+age=21
+print(name)
+# somenothing something
