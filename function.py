d9d7d6aa82fb7df66799ad6fe907c8105ff8d525
lourthuxavierm <xavierstayfoolish@gmail.com>
2023-07-27 12:15:56 +0530

    first push up

---
+++ function.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    """
+    This function takes two numbers as input and returns their sum.
+    """
+    return a + b
+
+# Test the function
+num1 = 5
+num2 = 10
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
